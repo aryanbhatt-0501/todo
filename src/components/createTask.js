8a60277272857502b4a30b9f40fc69a74390fa37
Aryan Bhatt <aryanbhatt@Aryans-MacBook-Air.local>
2024-10-03 19:01:57 +0530

    storing work before going OOO

--- src/components/createTask.js
+++ src/components/createTask.js
@@ -12,7 +12,30 @@ function CreateTask() {
                 Create a task
             </DialogTitle>
             <DialogContent>
-                <div className='dialogContent'>abcd</div>
+                <DialogContentText>
+                    Fill in the details below please
+                </DialogContentText>
+                <div className='dialogContent'>
+                    <div className='formContainer'>
+                        <form style={{width: '100%'}}>
+                            <label>
+                                Task description:
+                                <input type='text' style={{height: '20%', width: '50%'}}></input>
+                            </label>
+                            <br />
+                            <label>
+                                Assignee:
+                                <input placeholder='Assign this task to someone or yourself'></input>
+                                <a><p>Assign to me</p></a>
+                            </label>
+                            <br />
+                            <label>
+                                Due date:
+                                <input type='date'></input>
+                            </label>
+                        </form>
+                    </div>
+                </div>
             </DialogContent>
             <DialogActions sx={{justifyContent: 'space-around'}}>
                     <button>Cancel</button>
